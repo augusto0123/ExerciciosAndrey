0c39d8c3bf9a56c6611ec6d457a375a2056d34ed
Augusto <senescal.oliveira@gmail.com>
2025-02-08 10:29:47 -0300

    feat: adição do calculo da area e do perimetro

--- src/ExercicioDois/ExercicioDois.java
+++ src/ExercicioDois/ExercicioDois.java
@@ -6,5 +6,12 @@ public class ExercicioDois {
 
         double pi = 3.14;
         double raio = 10;
+
+        double area = pi * (raio * raio);
+
+        double perimetro = 2 * pi * raio;
+
+        System.out.println("A área é " + area);
+        System.out.println("O perímetro é " + perimetro);
     }
 }
