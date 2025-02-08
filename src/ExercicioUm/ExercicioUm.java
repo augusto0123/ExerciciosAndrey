51c7cc5e14af2afb02f7fa4bdc637c63c5c50175
Augusto <senescal.oliveira@gmail.com>
2025-02-08 10:15:24 -0300

    feat: adição do calculo da media de salario

--- src/ExercicioUm/ExercicioUm.java
+++ src/ExercicioUm/ExercicioUm.java
@@ -9,6 +9,11 @@ public class ExercicioUm {
         double salarioTres = 1600.40;
         double salarioQuatro = 5300.25;
         double salarioCinco = 2050.25;
-    }
 
+        double total = salarioUm + salarioDois + salarioTres + salarioQuatro + salarioCinco;
+
+        double media = total / 5;
+
+        System.out.printf("A média de salários desta empresa é R$ %.2f\n", media);
+    }
 }
