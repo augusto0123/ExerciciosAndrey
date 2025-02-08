84fe18e643d2ea1904ce0b9e6c141726095c9875
Augusto <senescal.oliveira@gmail.com>
2025-02-08 10:09:27 -0300

    feat: adição das variaveis do exercicio um

---
+++ src/ExercicioUm/ExercicioUm.java
@@ -0,0 +1,14 @@
+package ExercicioUm;
+
+public class ExercicioUm {
+
+    public static void executar(){
+
+        double salarioUm = 1800.00;
+        double salarioDois = 2850.00;
+        double salarioTres = 1600.40;
+        double salarioQuatro = 5300.25;
+        double salarioCinco = 2050.25;
+    }
+
+}
